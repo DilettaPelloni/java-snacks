17c1da72ea5a4eb0ab96a13116814eb1325603f7
Diletta Pelloni <dilpel@live.it>
2023-06-01 15:07:17 +0200

    Terminato snack9

---
+++ src/snacks/Snack9.java
@@ -0,0 +1,16 @@
+package snacks;
+
+public class Snack9 {
+    public static void main(String[] args) {
+
+        int TOTAL_NUMBERS = 10;
+        int sum = 0;
+        for (int i = 1; i <= TOTAL_NUMBERS ; i++) {
+            sum += i;
+        }
+        double avg = (double)sum / TOTAL_NUMBERS;
+
+        System.out.println("La somma dei primi " + TOTAL_NUMBERS + " numeri è: " + sum);
+        System.out.println("La media dei primi " + TOTAL_NUMBERS + " numeri è: " + avg);
+    }
+}
