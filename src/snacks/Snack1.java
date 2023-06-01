013bf1e49b200c989e801a8e2387caf737b764fb
Diletta Pelloni <dilpel@live.it>
2023-06-01 15:16:23 +0200

    Terminato snack1

---
+++ src/snacks/Snack1.java
@@ -0,0 +1,19 @@
+package snacks;
+
+//IMPORT
+import java.util.Scanner;
+
+//CONSEGNA
+//Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+public class Snack1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Inserisci un numero: ");
+        int num = scan.nextInt();
+        if(num % 2 == 0) {
+            System.out.println(num + " è pari, quindi stampo " + num);
+        } else {
+            System.out.println(num + " è dispari, quindi stampo " + (num + 1));
+        }
+    }
+}
